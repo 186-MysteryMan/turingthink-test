e2d63f631168e27aed1b44258d85672364ccd1da
盛攻杰 <765040854@qq.com>
2023-01-12 17:36:35 +0800

    feat:放开swagger注释

--- mq/src/main/java/com/turingthink/rabbit/config/SwaggerConfiguration.java
+++ mq/src/main/java/com/turingthink/rabbit/config/SwaggerConfiguration.java
@@ -1,10 +1,14 @@
 package com.turingthink.rabbit.config;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,6 +16,12 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @version v0.0.0
@@ -21,7 +31,7 @@ import springfox.documentation.spring.web.plugins.Docket;
  * @time: 2019-01-08 0008 下午 11:17
  * @description:
  */
-//@Configuration
+@Configuration
 @EnableOpenApi
 @Profile({"dev", "test"})
 public class SwaggerConfiguration {
